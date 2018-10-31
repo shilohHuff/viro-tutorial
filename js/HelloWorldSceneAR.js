f773cabb80eaa0a431aaee89517df2594844f0a8
Shiloh Huff <shilohhuff@gmail.com>
2018-10-30 22:31:47 -0500

    chore: playing iwth physics

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -8,6 +8,15 @@ import {
   ViroARScene,
   ViroText,
   ViroConstants,
+  ViroBox,
+  ViroMaterials,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroSpotLight,
+  ViroARPlaneSelector,
+  ViroARPlane,
+  ViroNode,
+  ViroAnimations,
 } from 'react-viro';
 
 export default class HelloWorldSceneAR extends Component {
@@ -28,6 +37,45 @@ export default class HelloWorldSceneAR extends Component {
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized} >
         <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+        <ViroAmbientLight color={"#aaaaaa"} />
+        <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]}
+          position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
+        <ViroARPlane
+          minHeight={.5}
+          minwidth={.5}
+        >
+          <ViroBox
+            position={[0,1,-3]}
+            height={1} width={1} length={1}
+            physicsBody={{
+              type:'Static', mass:0
+            }}
+          />
+          <ViroBox
+            position={[0,1,-3]}
+            height={1} width={1} length={1}
+            physicsBody={{
+              type:'Static', mass:0
+            }}
+          />
+          <ViroBox
+            position={[0,1,-3]}
+            height={1} width={1} length={1}
+            physicsBody={{
+              type:'Static', mass:0
+            }}
+          />
+        </ViroARPlane>
+        <ViroNode position={[0,-1,0]} dragType="FixedToWorld" onDrag={()=>{}} >
+          <Viro3DObject
+                    source={require('./res/emoji_smile/emoji_smile.vrx')}
+                    resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
+                        require('./res/emoji_smile/emoji_smile_normal.png'),
+                        require('./res/emoji_smile/emoji_smile_specular.png')]}
+                    position={[-.5, .5, -1]}
+                    scale={[.2, .2, .2]}
+                    type="VRX" />
+        </ViroNode>
       </ViroARScene>
     );
   }
@@ -42,6 +90,21 @@ export default class HelloWorldSceneAR extends Component {
     }
   }
 }
+
+ViroMaterials.createMaterials({
+  grid: {
+    diffuseTexture: require('./res/grid_bg.jpg'),
+  },
+});
+
+ViroAnimations.registerAnimations({
+  rotate: {
+    properties: {
+      rotateY: "+=90"
+    },
+    duration: 250, //.25 seconds
+  },
+});
 
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
